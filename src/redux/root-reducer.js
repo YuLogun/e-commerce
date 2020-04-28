bcf6b6daf596eb40f10ec4bdf642368c43575957
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-04-28 11:53:35 +0400

    connected Shop component to Redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,11 +5,13 @@ import cartReducer from './cart/cart-reducer'
 import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import directoryReducer from './directory/directory-reducer'
+import shopReducer from './shop/shop-reducer'
 
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
-  directory: directoryReducer
+  directory: directoryReducer,
+  shop: shopReducer
 })
 
 const persistConfig = {
