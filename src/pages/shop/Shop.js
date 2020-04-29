5070b482365b09999d2aaedb6fed2b548bd7ddfa
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-04-29 09:43:19 +0400

    started creating dynamic routes

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import CollectionsOverview from '../../components/collections-overview/CollectionsOverview'
 
-const ShopPage = ({ collections }) => {
+import { Route } from 'react-router-dom'
+
+import CollectionsOverview from '../../components/collections-overview/CollectionsOverview'
+import CollectionPage from '../collection/CollectionPage'
+
+const ShopPage = ({ match }) => {
+  console.log(match)
   return (
     <div className="shop-page">
-      <CollectionsOverview />
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   )
 }
--- src/redux/shop/shop-selectors.js
+++ src/redux/shop/shop-selectors.js
@@ -6,3 +6,18 @@ export const selectShopCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
 )
+
+const COLLECTION_ID_MAP = {
+  hoodies: 1,
+  socks: 2,
+  womens: 3,
+  mens: 4,
+  scarves: 5
+}
+
+export const selectCollection = (collectionUrlParams) =>
+  createSelector([selectShopCollections], (shopCollections) =>
+    shopCollections.find(
+      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParams]
+    )
+  )
